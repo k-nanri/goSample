8b09b1e5f37573a3d4243336ec70bd57cfdc3c82
k-nanri <kotaro.nanri15@gmail.com>
2021-06-28 08:04:01 +0900

    6/28 update

--- src/dbsample/main/main.go
+++ src/dbsample/main/main.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"net/http"
+	"time"
 )
 
 var db *sql.DB
@@ -22,8 +24,34 @@ func logFatal(err error) {
 func slowQuery() error {
 	_, err := db.Exec("SELECT pg_sleep(5)")
 	return err
+}
+
+func slowHandler(w http.ResponseWriter, req *http.Response) {
+	start := time.Now()
+	err := slowQuery()
+	if err != nil {
+		log.Printf("Error : %s\n", err.Error())
+		return
+	}
+	fmt.Fprintln(w, "OK")
+	fmt.Printf("slowHandler took: %v\n", time.Since(start))
+}
 
 func main() {
 
-	fmt.Println("Start")
+	var err error
+	db, err = sql.Open("postgres", conn)
+	logFatal(err)
+
+	err = db.Ping()
+	logFatal(err)
+
+	srv := http.Server {
+		Addr: ":8080",
+		WriteTimeout:2 * time.Second,
+		Handler: http.HandlerFunc(slowHandler),
+	}
+
+	log.Println("STart Http Server...")
+	log.Fatal(srv.ListenAndServe())
 }
